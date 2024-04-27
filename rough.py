6ab4eb42ab8a47797c441edf30b7c322ea9f3018
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-04-27 17:54:40 +0600

    completed upto the context manager'

--- rough.py
+++ rough.py
@@ -357,8 +357,7 @@
 # parser.add_argument(
 #   '-c', '--color', metavar="color",required=True, choices={"red", "blue"}, help="the color to search for")
 
-# args = parser.parse_args
-()
+# args = parser.parse_args()
 
 # print("the color is " + args.color)
 
@@ -455,7 +454,7 @@
 # print(factorial(5))
 
 
-## decorators | a function that wraps another function and enhances it | it is a function that takes another function as an argument and returns a new function | it is a way to modify the behavior of a function without modifying its source code 
+## Decorators | a function that wraps another function and enhances it | it is a function that takes another function as an argument and returns a new function | it is a way to modify the behavior of a function without modifying its source code 
 
 # def logtime(func):
 #   def wrapper():
@@ -466,20 +465,167 @@
 #   return wrapper
 
 
-def logtime(func):
-    def wrapper():
-        # do something before
-        print("before")
-        val = func()
-        # do something after
-        print("after")
-        return val
-    return wrapper
+# def logtime(func):
+#   def wrapper():
+#     # do something before
+#     print("before")
+#     val = func()
+#     # do something after
+#     print("after")
+#     return val
+#   return wrapper
 
-@logtime
-def sayHello():
-    x = 10
-    print(f"Hello! {x}")
+# @logtime
+# def sayHello():
+#   x = 10
+#   print(f"Hello! {x}")
 
 
-sayHello()
+# sayHello()
+
+
+## Docstring | a way to add documentation to your functions | it is a string that appears as the first expression in a function or class | start and end with three double quotes | can be used to document the function, class, module, or program
+
+# """Dog module
+# This is a Dog function with dog properties
+# """
+# def dog():
+#   """This function prints a dog"""
+#   print("Woof! Woof!")
+
+# print(help(dog))
+
+## Annotations | a way to add type hints to your functions | it is a way to document the type | python ignores the type and by default interprets the type of the variable | it is not enforced by the language | separate tool like <mypy> can be used to check the type of the variable
+
+# def count(num: int) -> int:
+#   total: int = 0
+#   for n in range(1, num + 1):
+#     total += n
+  
+#   return total
+
+# print(count(10))
+
+
+
+## Exception | a way to handle errors | it is a way to handle errors in your code | it is a way to grace | <raise> use to raise an exception manually | <try> use to try something | <except> use to handle the exception | <finally>
+# try:
+#   def divide(num: int) -> int:
+#     return int(10 / num)
+
+#   def countdown(num: int) -> int:
+#     total: int = 0
+#     for n in range(1, num + 1):
+#       total += n
+      
+#     print(total)
+#     return total
+
+#   def main():
+#     try:
+#       total = 0
+#       total = countdown(10)
+#       total = divide(0)
+#     except TypeError:
+#       print(f"A {TypeError} was raised 1")
+#     except ZeroDivisionError:
+#       raise Exception("Division by Zero Error occurred")
+#     except:
+#       print(f"An unknown error was raised")
+#     else:
+#       print(f"The result is {divide(2)}")
+#     finally:
+#       print("This will always execute")
+
+#   main()
+
+# except NameError as e:
+#   print(f"A {e} was raised 2")
+# except Exception as err:
+#   print(f"An unknown error {err} was raised 2")
+
+
+
+## Custom Exception class | 
+
+## <pass>| a way to do nothing | when a function/method is empty | when a class without method | it is a way to do nothing | it is a way to pass the execution to the next line | it is a way to skip the execution of the current line | it is a way to skip the execution of the current block of code
+# class DogNotFoundException(Exception):
+#   print("Inside Dog Not Found Exception")
+#   pass
+
+# try:
+#   raise DogNotFoundException()
+# except DogNotFoundException:
+#   print("Dog not found!")
+
+
+
+## <with> | a way to open and close a file | with will close the file automatically | it handles Exception implicitly 
+
+# #without with
+
+# try:
+#   filepath = ("./user.txt")
+#   file = open(filepath, "r")
+#   content = file.read()
+#   print(content)
+# finally:
+  # file.close()
+
+# with <with>
+# filename = ("./user.txt")
+
+
+
+##The <with> statement in Python is used to wrap the execution of a block with methods defined by a context manager. It allows for the acquisition and release of resources such as files or network connections. When a context manager is used with a with statement, it ensures that necessary cleanup is done at the end of the block.
+
+# # Opening and closing files:
+
+# with open(filename, "r") as file:
+#   content = file.read()
+#   print(content)
+
+
+
+# # Database connection
+# # The database connection is automatically closed after the block ends
+
+# import sqlite3
+# with sqlite3.connect("example.db") as conn:
+#     cursor = conn.cursor()
+#     cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+
+
+# #Locking and unlocking resources:
+
+# import threading
+# lock = threading.Lock()
+# with lock:
+#     # critical section of code where only one thread can execute at a time
+#     pass
+# # The lock is automatically released after the block ends
+
+
+## Context Manager | 
+## A context manager in Python is an object that defines __enter__ and __exit__ methods to enable the with statement to be used with the object. The __enter__ method is called when execution enters the context of the with statement, and the __exit__ method is called when execution exits the context, even if an exception occurs.
+
+## a way to manage resources | it is a way to manage resources such as files, network connections, and database connections 
+
+## This example demonstrates how custom context managers can be used to handle resources, clean up tasks, and manage exceptions effectively within a with statement.
+class CustomContextManager:
+    def __enter__(self):
+        print("Entering the context")
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        print(f"Exiting the context with {exc_type}")
+
+        if exc_type is not None:
+            print(f"An Exception of type: {exc_type} occurred with the value {exc_value}")
+        
+        return True
+
+with CustomContextManager() as manager:
+    print("Inside the context")
+    raise ValueError("An error occurred inside the context")
+    print(2+2)
